8005876a02d3b63f134b2365d7971966556e97bd
Mo7medFakhry <mohamedhawas415@gmail.com>
2025-03-29 01:07:45 +0200

    update in magration

--- database/migrations/2025_03_28_222121_create_followers_table.php
+++ database/migrations/2025_03_28_222121_create_followers_table.php
@@ -2,19 +2,22 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+
 return new class extends Migration {
     public function up(): void
     {
-        Schema::create('followers', function (Blueprint $table) {
+        Schema::create('follows', function (Blueprint $table) {
             $table->id();
             $table->foreignId('follower_id')->constrained('users')->onDelete('cascade');
             $table->foreignId('following_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
-            $table->unique(['follower_id', 'following_id']);
+
+            $table->unique(['follower_id', 'following_id']); // Prevent duplicate follows
         });
     }
+
     public function down(): void
     {
-        Schema::dropIfExists('followers');
+        Schema::dropIfExists('follows');
     }
 };
